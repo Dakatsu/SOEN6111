4f9c58d8bb1f8340ab1b19fe203e6572ab398755
chaoukic <sammychaouki25@gmail.com>
2022-03-06 15:33:49 -0500

    Add a init script that will build the game dataset

---
+++ data/scripts/init_script.py
@@ -0,0 +1,57 @@
+import json
+import requests
+import time 
+import pandas as pd
+import os
+
+url_to_get_all_apps = "https://api.steampowered.com/ISteamApps/GetAppList/v0002/?format=json"
+url_to_get_app_information="https://store.steampowered.com/api/appdetails?appids="
+dictionnary = {}
+path = os.getcwd()
+directory_path=os.path.abspath(os.path.join(path, os.pardir))
+
+def generateData():
+    response = requests.get(url_to_get_all_apps)
+    time.sleep(2.5)
+    jsonObjectArray = response.json()['applist']['apps']
+    for app in jsonObjectArray:
+        appid = str(app['appid'])
+        fetch_url = url_to_get_app_information+appid
+        response = requests.get(fetch_url).json()[appid]
+        if response['success']:
+            dataVal = response['data']
+            if dataVal['type'] == 'game':
+                if dataVal['is_free']:
+                    price = 0
+                else: 
+                    if 'price_overview' in dataVal:
+                        price = dataVal['price_overview']['initial']/100
+                    else:
+                        price = 0
+                if  "metacritic" not in dataVal:
+                    metacritic = 0
+                else:
+                    metacritic = dataVal['metacritic']['score']
+                if "recommendation" not in dataVal:
+                    continue
+                dictionnary[appid] = {
+                    "name": dataVal['name'],
+                    "price":price,
+                    "required_age": dataVal['required_age'],
+                    "developers": ','.join([str(elem) for elem in dataVal['developers']]),
+                    "publishers": ','.join([str(elem) for elem in dataVal['publishers']]),
+                    "rating": metacritic,
+                    "genres":','.join([str(elem['description']) for elem in dataVal['genres']]),
+                    "release_date": dataVal['release_date']['date'],
+                    "numberOfRecommendations": dataVal['recommendations']['total']
+                }
+        time.sleep(5)
+    df = pd.DataFrame.from_dict(dictionnary, orient='index')
+    df.to_csv(directory_path + '/datasets/games.csv') 
+
+
+
+
+
+if __name__ == '__main__':
+    generateData()
