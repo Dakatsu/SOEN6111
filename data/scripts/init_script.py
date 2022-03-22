f966cdf4475dfcd469bdb7a181fc2007e13ccaee
chaoukic <sammychaouki25@gmail.com>
2022-03-21 20:40:48 -0400

    Improve data retrieval

--- data/scripts/init_script.py
+++ data/scripts/init_script.py
@@ -1,5 +1,4 @@
 import json
-import requests
 import time 
 import pandas as pd
 import os
@@ -9,48 +8,43 @@ url_to_get_app_information="https://store.steampowered.com/api/appdetails?appids
 dictionnary = {}
 path = os.getcwd()
 directory_path=os.path.abspath(os.path.join(path, os.pardir))
+parentPath = os.path.abspath(os.path.join(directory_path,os.pardir))
+
 
 def generateData():
-    response = requests.get(url_to_get_all_apps)
-    time.sleep(2.5)
-    jsonObjectArray = response.json()['applist']['apps']
-    for app in jsonObjectArray:
-        appid = str(app['appid'])
-        fetch_url = url_to_get_app_information+appid
-        response = requests.get(fetch_url).json()[appid]
-        if response['success']:
-            dataVal = response['data']
-            if dataVal['type'] == 'game':
-                if dataVal['is_free']:
-                    price = 0
-                else: 
-                    if 'price_overview' in dataVal:
-                        price = dataVal['price_overview']['initial']/100
-                    else:
-                        price = 0
-                if  "metacritic" not in dataVal:
-                    metacritic = 0
-                else:
-                    metacritic = dataVal['metacritic']['score']
-                if "recommendation" not in dataVal:
-                    continue
-                dictionnary[appid] = {
-                    "name": dataVal['name'],
+    with open('successful_appIDs.txt') as file:
+        valid_appIds = [app_id.strip() for app_id in file.readlines()]
+
+    for app_id in valid_appIds:
+        json_obj = json.load(open(path+"/appdetails/appID_" + app_id+'.json'))
+        if 'type' not in json_obj or json_obj['type']!= 'game' or 'developers' not in json_obj or 'required_age' not in json_obj or 'publishers' not in json_obj or 'genres' not in json_obj:
+            continue
+        if json_obj['is_free'] == False and 'price_overview' in json_obj :
+            price = json_obj['price_overview']['final']/100
+        else:
+            price = 0
+        dictionnary[app_id] = {
+                    "name": json_obj['name'],
+                    "free_to_play":json_obj['is_free'],
                     "price":price,
-                    "required_age": dataVal['required_age'],
-                    "developers": ','.join([str(elem) for elem in dataVal['developers']]),
-                    "publishers": ','.join([str(elem) for elem in dataVal['publishers']]),
-                    "rating": metacritic,
-                    "genres":','.join([str(elem['description']) for elem in dataVal['genres']]),
-                    "release_date": dataVal['release_date']['date'],
-                    "numberOfRecommendations": dataVal['recommendations']['total']
-                }
-        time.sleep(5)
+                    "required_age": json_obj['required_age'],
+                    "developers": '-'.join([str(elem) for elem in json_obj['developers']]),
+                    "publishers": '-'.join([str(elem) for elem in json_obj['publishers']]),
+                    "genres":'-'.join([str(elem['description']) for elem in json_obj['genres']]),
+                    "release_date": json_obj['release_date']['date'],
+        }
+    
     df = pd.DataFrame.from_dict(dictionnary, orient='index')
-    df.to_csv(directory_path + '/datasets/games.csv') 
+    df.index.name = 'appID'
+    path_to_write = './data/datasets'
+    if not os.path.exists(path_to_write):
+        os.mkdir('./data/datasets')
+    
+    df.to_csv('./data/datasets/games.csv') 
 
 
-
+def fetchMissingData(dataframe:pd.DataFrame):
+    return dataframe
 
 
 if __name__ == '__main__':
